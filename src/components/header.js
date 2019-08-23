c47e0541b4f7e944a1f08223c4a7233a97981526
Jordan <jordandoan@hotmail.com>
2019-08-23 11:06:43 -0700

    Styling header. Elements are vertically centered

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,21 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import styles from "./header.module.less"
+import styles from "./styles/header.module.less"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <Link className={styles.link} to="/preface">Preface</Link>
+  <header className={styles.header}>
+    <div className={styles.container}>
+      <div>
+        <h1 className={styles.title}>
+          <Link className={styles.link} to="/">
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+      <div className={styles.links}>
+        <Link className={styles.link} to="/preface">Preface</Link>
+      </div>
     </div>
   </header>
 )

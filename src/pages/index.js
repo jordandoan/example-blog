51cfbdd309e431f765726d4843e37506af3959f9
Jordan <jordandoan@hotmail.com>
2019-08-23 17:35:41 -0700

    Finalized fonts for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,17 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import styles from "../components/styles/content.module.less";
 const IndexPage = ({ data }) => {
   return (
   <Layout>
     <SEO title="Home"/>
-    <h2>Why Won't You Work</h2>
-      <h3>Posts</h3>
+    <h2 className={styles.title}>Why Won't You Work</h2>
+      <h3 className={styles.title}>Posts</h3>
       {data.allWordpressPost.edges.map(({ node }) => (
         <div>
           <Link to={node.slug}>
-          <h4>{node.title}</h4>
+          <h4 className={styles.title}>{node.title}</h4>
           </Link>
           <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
         </div>
